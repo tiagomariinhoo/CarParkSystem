b99f1684c196af4711b7c4a4149b2cdee0bf20c5
tiagomariinhoo <tiago.marinho.797@gmail.com>
2017-05-14 17:34:16 -0300

    Finish

--- src/ParkingSlotsMemento.java
+++ src/ParkingSlotsMemento.java
@@ -1,17 +1,39 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class ParkingSlotsMemento {
-    protected VehiclePrototype[] vehicleParkingSlots = new VehiclePrototype[20];
+    private VehiclePrototype[] states = new VehiclePrototype[20];
+    private List<Integer> states2;
     
     public ParkingSlotsMemento(){
-    
     }
     
-    public void setVehicleArray(VehiclePrototype[] vehicleArray){
-        this.vehicleParkingSlots = vehicleArray;
+    public void setVehicleArray(VehiclePrototype[] vehicleArray, List<Integer> vehicleList){
+        /*System.out.println("Vehicle array : ");
+        System.out.println(Arrays.asList(vehicleArray));
+        System.out.println("vehicle List : ");
+        System.out.println(Arrays.asList(vehicleList));*/
+        this.states = vehicleArray;
+        this.states2 = vehicleList;
+        /*System.out.println("States array : ");
+        System.out.println(Arrays.asList(states));
+        System.out.println("States2 List : ");
+        System.out.println(Arrays.asList(states2));*/
     }
     
-    public VehiclePrototype[] getParkingSlots(){
-        return vehicleParkingSlots;
+    public VehiclePrototype[] getUltimoSalvo(){
+        System.out.println("Get ultimo array : ");
+        System.out.println(Arrays.asList(states));
+        return states;
+    }
+    
+    public List<Integer> getUltimoInteger(){
+        System.out.println("Get ultimo list : ");
+        System.out.println(Arrays.asList(states2));
+        return states2;
     }
     
 }
